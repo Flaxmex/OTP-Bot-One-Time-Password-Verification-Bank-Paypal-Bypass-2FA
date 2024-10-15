dffef5e7efa4f14a73d028d9cf2558af89abc3d4
Andrews23443 <dvgdsgf34@outlook.com>
2024-10-15 21:38:48 +0400

    Upload Files

---
+++ SecureOTP-2FA-Bypass/internal/entity/states.go
@@ -0,0 +1,10 @@
+package entity
+
+var (
+	StateAddUserEmail  = "addingUserEmail"  // For AddUser
+	StateAddUserChatID = "insertingChatID"  // For AddUser
+	StateAddOrigin     = "addingOrigin"     // For AddOrigin
+	StateAddPermission = "addingPermission" // For AddPermission
+	StateChooseUser    = "choosingUser"     // For AddPermission
+	StateChooseHost    = "choosingHost"     // For AddPermission
+)
